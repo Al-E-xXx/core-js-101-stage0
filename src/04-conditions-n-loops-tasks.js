d1818a5529d07cd86b92cb791985b9eab478b69e
alex <vurdalackoff@gmail.com>
2025-09-07 01:18:51 +0300

    feat: solve getFizzBuzz

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -26,8 +26,22 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+
+  return num;
+
+  // throw new Error('Not implemented');
 }
 
 /**
