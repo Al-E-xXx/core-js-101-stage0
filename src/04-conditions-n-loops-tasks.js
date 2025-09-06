b43d2f1259e18adf868bc8f73f054629de18a0fb
alex <vurdalackoff@gmail.com>
2025-09-07 01:56:19 +0300

    feat: solve isInsideCircle

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -177,8 +177,12 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const dx = point.x - circle.center.x;
+  const dy = point.y - circle.center.y;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+  return distance < circle.radius;
+  // throw new Error('Not implemented');
 }
 
 /**
